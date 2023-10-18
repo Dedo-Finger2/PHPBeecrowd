a8bbae3ca469c18a65c56d88921cd4e2d669122e
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-18 12:32:35 -0700

    docblocks added

--- App/Challenges/Challenge.php
+++ App/Challenges/Challenge.php
@@ -2,6 +2,9 @@
 
 namespace App\Challenges;
 
+/**
+ * Classe de desafios
+ */
 class Challenge
 {
     private static int $CHALLENGENUMBER = 1000;
@@ -14,40 +17,73 @@ class Challenge
     public function __construct(string $title)
     {
         $this->challengeTitle = $title;
-
         $this->entriesOutputs = new EntryOutput();
     }
 
 
+    /**
+     * getChallengeInfo
+     * 
+     * Este método retorna um texto formatado contendo informações do desafio sendo feito
+     *
+     * @return void
+     */
     public function getChallengeInfo(): void
     {
-        echo "<br><br><br>";
+        echo "<br><br><br>";        // Quebra de linha
+        echo "<hr>";                // Linha divisora
 
-        echo "<hr>";
-        echo self::$CHALLENGENUMBER . " - " . $this->challengeTitle;
-        
-        echo "<h2>Entry => Output</h2>";
+        echo self::$CHALLENGENUMBER . " - " . $this->challengeTitle;        // Mostra o número do desafio e seu nome
+
+        echo "<h2>Entry => Output</h2>";                                    // Mostra as entradas e saídas deste desafio
         foreach ($this->entriesOutputs->getEntriesOutputs() as $entry => $output) {
             echo "<li>". $entry . " => " . $output . "</li>";
         }
 
-        echo "<hr>";
+        echo "<hr>";                // Linha divisora
 
-        self::$CHALLENGENUMBER++;
+        self::$CHALLENGENUMBER++;   // Incrementa o número do desafio
     }
 
 
+    /**
+     * getUserOutput
+     * 
+     * Este método retorna o output do usuário
+     *
+     * @return mixed
+     */
     public function getUserOutput(): mixed
     {
         return $this->userOutput;
     }
 
 
+    /**
+     * setUserOutput
+     * 
+     * Este método da valor ao output do usuário
+     *
+     * @param mixed $value Valor que será inserido no output do usuário
+     * @return void
+     */
     public function setUserOutput(mixed $value)
     {
         $this->userOutput = $value;
     }
 
+
+
+    /**
+     * checkUserOutput
+     * 
+     * Método responsável por validar o output obtido pelo usuário com uma determinada entrada
+     * é comparado a entrada e output do usuário com a entrada correspondente e output correspondente
+     * no objeto do tipo EntryOutout.
+     *
+     * @param mixed $userEntry Valor de entrada do usuário
+     * @return string Indicação se o usuário concluiu o desafio ou não
+     */
     public function checkUserOutput(mixed $userEntry): string
     {
         foreach ($this->entriesOutputs->getEntriesOutputs() as $entry => $output) {
--- App/Challenges/EntryOutput.php
+++ App/Challenges/EntryOutput.php
@@ -7,16 +7,45 @@ class EntryOutput
     private array $entries;
     private array $outputs;
 
+
+    /**
+     * addEntries
+     * 
+     * Este método adiciona uma nova entrada no array de entradas, recebendo um número
+     * ilimitado de parâmetros, transformando eles em um array
+     *
+     * @param mixed ...$entries Entradas que o usuário vai inserir
+     * @return void
+     */
     public function addEntries(mixed ...$entries)
     {
         $this->entries = $entries;
     }
 
+
+    /**
+     * addOutputs
+     * 
+     * Este método adiciona uma nova saída no array de saídas, recebendo um número
+     * ilimitado de parâmetros, transformando eles em um array
+     *
+     * @param mixed ...$outputs saídas que o usuário vai inserir
+     * @return void
+     */
     public function addOutputs(mixed ...$outputs)
     {
         $this->outputs = $outputs;
     }
 
+
+    /**
+     * getEntriesOutputs
+     * 
+     * Este método retorna todas as entradas e saídas em um novo array associativo
+     * onde a chave é a entrada e o valor é a saída
+     *
+     * @return array Array associativo com as entradas e saídas
+     */
     public function getEntriesOutputs(): array
     {
         $entriesOutputs = [];
@@ -26,7 +55,6 @@ class EntryOutput
         }
 
         return $entriesOutputs;
-
     }
 
 }
